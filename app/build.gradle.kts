9fb68812543fce1b7e54e0ecc7abb5c764f0de11
Alban <aldal18@student.sdu.dk>
2024-11-16 17:26:55 +0100

    mainMenu commit fix final version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material:material:1.7.5")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
