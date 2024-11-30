8f6982a0d9d9cb6d3523adc052663cb901422af0
Daniel <danihansen74@gmail.com>
2024-11-30 22:05:49 +0100

    cleaned up logos and colors on folkedex page and party pages

--- app/src/main/java/com/example/folkedex/ui/theme/Color.kt
+++ app/src/main/java/com/example/folkedex/ui/theme/Color.kt
@@ -14,49 +14,49 @@ val black = Color(0xFF000000)
 
 
 val ModeraterneBackground = Color(0xFF842990)
-val ModeraterneButton = Color(0xFF9F5BBA)
+val ModeraterneButton = Color(0xFF842990)
 
 val VenstreBackground = Color(0xFF0047D2)
-val VenstreButton = Color(0xFF336FE0)
+val VenstreButton = Color(0xFF0047D2)
 
-val SocialdemokratietBackground = Color(0xFFF23A3A)
-val SocialdemokratietButton = Color(0xFFF55C5C)
+val SocialdemokratietBackground = Color(0xFFEC4C45)
+val SocialdemokratietButton = Color(0xFFEC4C45)
 
-val KonservativBackground = Color(0xFF008000)
-val  KonservativButton = Color(0xFF20d420)
+val KonservativBackground = Color(0xFF085742)
+val  KonservativButton = Color(0xFF085742)
 
-val SfBackground = Color(0xFFd42020)
-val  SfButton = Color(0xFFe34444)
+val SfBackground = Color(0xFFA30000)
+val  SfButton = Color(0xFFA30000)
 
-val RadikalevenstreBackground = Color(0xFF0000FF)
-val  RadikalevenstreButton = Color(0xFF336FE0)
+val RadikalevenstreBackground = Color(0xFFE61783)
+val  RadikalevenstreButton = Color(0xFFE61783)
 
-val DanskfolkepartiBackground = Color(0xFF0000FF)
-val  DanskfolkepartiButton = Color(0xFF336FE0)
+val DanskfolkepartiBackground = Color(0xFF163A5E)
+val  DanskfolkepartiButton = Color(0xFF163A5E)
 
-val SambandspartietBackground = Color(0xFF0080FF)
-val  SambandspartietButton = Color(0xFF4DA6FF)
+val SambandspartietBackground = Color(0xFF006CB4)
+val  SambandspartietButton = Color(0xFF006CB4)
 
-val DanmarksdemokraterneBackground = Color(0xFFF23A3A)
-val  DanmarksdemokraterneButton = Color(0xFFF55C5C)
+val DanmarksdemokraterneBackground = Color(0xFF2474B7)
+val  DanmarksdemokraterneButton = Color(0xFF2474B7)
 
-val AlternativetBackground = Color(0xFF39FF14)
-val  AlternativetButton = Color(0xFF79FF61)
+val AlternativetBackground = Color(0xFF00FF00)
+val  AlternativetButton = Color(0xFF00FF00)
 
-val InuitBackground = Color(0xFFFF0000)
-val  InuitButton = Color(0xFFE34444)
+val InuitBackground = Color(0xFFB01116)
+val  InuitButton = Color(0xFFB01116)
 
-val LaBackground = Color(0xFFF5F5DC)
-val  LaButton = Color(0xFFE4E4A1)
+val LaBackground = Color(0xFF08304B)
+val  LaButton = Color(0xFF08304B)
 
-val JavnBackground = Color(0xFFFF3434)
-val  JavnButton = Color(0xFFF55C5C)
+val JavnBackground = Color(0xFFA70101)
+val  JavnButton = Color(0xFFA70101)
 
-val SiumutBackground = Color(0xFF0000FF)
-val  SiumutButton = Color(0xFF336FE0)
+val SiumutBackground = Color(0xFF345995)
+val  SiumutButton = Color(0xFF345995)
 
-val EnhedslistenBackground = Color(0xFFF23A3A)
-val  EnhedslistenButton = Color(0xFFF55C5C)
+val EnhedslistenBackground = Color(0xFFD10852)
+val  EnhedslistenButton = Color(0xFFD10852)
 
 val altSocialdemokratiet = Color(0xFFEC4C45)
 val altVenstre = Color(0xFF092F93)
@@ -65,14 +65,14 @@ val altKonservative = Color(0xFF085742)
 val altEnhedslisten = Color(0xFFD10852)
 val altDanskFolkeparti = Color(0xFF163A5E)
 val altDanmarksdemokraterne = Color(0xFF2474B7)
-val altSocialistiskFolkeparti = Color(0xFFBBC2CC)
-val altRadikaleVenstre = Color(0xFFEE0890)
-val altAlternativet = Color(0xFFB9E6A5)
+val altSocialistiskFolkeparti = Color(0xFFA30000)
+val altRadikaleVenstre = Color(0xFFE61783)
+val altAlternativet = Color(0xFF00FF00)
 val altModeraterne = Color(0xFF872F93)
-val altSiumut = Color(0xFFBBC2CC)
-val altInuitAtaqatigiit = Color(0xFFB2181D)
-val altSambandsflokkurin = Color(0xFF0871B6)
-val altJavnaðarflokkurin = Color(0xFFB62734)
+val altSiumut = Color(0xFF345995)
+val altInuitAtaqatigiit = Color(0xFFB01116)
+val altSambandsflokkurin = Color(0xFF006CB4)
+val altJavnaðarflokkurin = Color(0xFFA70101)
 
 
 
