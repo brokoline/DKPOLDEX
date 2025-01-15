b6e96ee01c32c17922180883c83650d369aaf0bf
brokoline <125354647+brokoline@users.noreply.github.com>
2025-01-15 12:56:26 +0100

    Første forsøg - virker-ish!!!!! (undtagen socialdemokratiet)

--- app/src/main/java/com/example/folkedex/ui/party/PartyScreen.kt
+++ app/src/main/java/com/example/folkedex/ui/party/PartyScreen.kt
@@ -2,18 +2,7 @@ package com.example.folkedex.ui.theme
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -22,7 +11,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -123,6 +111,37 @@ fun Party(partyData: PartyData, onBackClick: () -> Unit = {}, navController: Nav
                             navController = navController,
                             onClick = { navController.navigate("com/example/folkedex/ui/history/${partyData.path}") }
                         )
+                    }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        CustomButton(
+                            text = "Policies",
+                            textColor = partyData.backColor,
+                            buttonColor = partyData.buttonColor,
+                            modifier = Modifier.width(170.dp),
+                            partyData = partyData,
+                            navController = navController,
+                            onClick = { navController.navigate("policies") }
+                        )
+                        CustomButton(
+                            text = "Statistics",
+                            textColor = partyData.backColor,
+                            buttonColor = partyData.buttonColor,
+                            modifier = Modifier.width(170.dp),
+                            partyData = partyData,
+                            navController = navController,
+                            onClick = { }
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+
                     }
                 }
             }
